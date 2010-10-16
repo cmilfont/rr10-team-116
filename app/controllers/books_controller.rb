93a9087a8e8ed9fb100e6301b4c005e6c5d668ed
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2010-10-16 18:20:59 -0300

    [#5718344] Adicionando partes da feature

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,18 @@ class BooksController < ApplicationController
       format.html
     end
   end
+  
+  def create
+    @book = Book.new(params[:book])
+    respond_to do |format|
+      if @book.save
+        flash[:notice] = 'Book saved successfully!'
+        format.html { redirect_to(@book) }
+      else
+        format.html { render :action => "new" }
+      end
+    end 
+  end
 
 end
 
