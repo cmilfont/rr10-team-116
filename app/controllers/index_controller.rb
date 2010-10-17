0eb10bf51cd264952be3948f219af7cfdaf474fa
razenha <rubem.azenha@gmail.com>
2010-10-17 21:09:26 -0200

    tag

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,15 @@
 class IndexController < ApplicationController
   def index
+    @tags = Book.tag_counts_on(:tags)
+  end
+  
+  def tag
+    tag = params[:id]
+    @books = Book.tagged_with(tag)
+    @tags = Book.tag_counts_on(:tags)
+    
+    render :controller => :books, :action => :index
+    
+    
   end
 end
