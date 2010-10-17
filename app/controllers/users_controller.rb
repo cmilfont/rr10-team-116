0768ce5d34b5c29521f37462bb633224987b58e9
razenha <rubem.azenha@gmail.com>
2010-10-17 17:03:51 -0200

    sdas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,30 @@
 class UsersController < ApplicationController
   def index
-    @user = current_user
-    @books = Book.all
+#    @user = current_user
+ #   @books = Book.all
 
-    render :action => "edit"
+  #  render :action => "edit"
+    @users = User.all.paginate(:page => params[:page], :per_page => 10)
+  
   end
+  
+  def edit
+    puts "******** #{params[:id]} +  + #{current_user.id}"
+    if params[:id] != current_user.id.to_s
+      flash[:error] = "You can't edit another user's profile"
+      redirect_to '/'
+    else
+      @user = User.find(params[:id])
+    end 
+    
+    
+  end
+  
+  def show
+    @user = User.find(params[:id])
+
+  end
+  
   
   def update
     @user = User.find(params[:id])
